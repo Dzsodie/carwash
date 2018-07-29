90dc0be7f8ffe83df4d7bd1e85fc2666e288450e
Mark Szabo <v-maszab@microsoft.com>
2018-07-29 16:00:00 +0200

    Tinkering with MSAL

--- src/MSHU.CarWash.PWA/ClientApp/src/App.js
+++ src/MSHU.CarWash.PWA/ClientApp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
+import { AzureAD, LoginType, MsalAuthProviderFactory } from 'react-aad-msal';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Layout from './components/Layout';
 import { Home } from './components/Home';
@@ -32,11 +33,44 @@ const theme = createMuiTheme({
 });
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userInfo: null
+        };
+    }
+
     displayName = App.name
+    
+    unauthenticatedFunction = loginFunction => {
+        if (!this.state.userInfo) loginFunction();
+        return (<div />);
+    };
+
+    userJustLoggedIn = userInfo => {
+        this.setState({ userInfo });
+    }
+
+    authenticatedFunction = logout => {
+        return (<div><button onClick={() => { logout(); }}>Logout</button></div>);
+    }
 
     render() {
         return (
             <MuiThemeProvider theme={theme}>
+                <AzureAD
+                    provider={new MsalAuthProviderFactory({
+                        authority: 'https://login.microsoftonline.com/common',
+                        clientID: '8797ccc8-ec4c-4483-a8e4-b6427bcc0584',
+                        scopes: ['openid', 'email', 'profile', 'offline_access'],
+                        type: LoginType.Redirect,
+                        persistLoginPastSession: true,
+                    })}
+                    unauthenticatedFunction={this.unauthenticatedFunction}
+                    userInfoCallback={this.userJustLoggedIn}
+                    authenticatedFunction={this.authenticatedFunction}
+                />
                 <Layout>
                     <Route exact path="/" component={Home} navbarName="My reservations" />
                     <Route exact path="/index.html" component={Home} navbarName="My reservations" />
